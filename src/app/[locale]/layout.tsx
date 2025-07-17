4f526f07f863504595fab649d62d68b92a500e26
khadizajarin <khadiza131310@gmail.com>
2025-07-17 20:43:56 +0600

    meta tag fixed for deployment

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,33 +3,39 @@ import {notFound} from 'next/navigation';
 import {routing} from './../../i18n/routing';
 import '../../app/globals.css';
 import type { Metadata } from 'next';
-import { getMessages } from 'next-intl/server';
 
-export const generateMetadata = async ({ params }: { params: { locale: string } }): Promise<Metadata> => {
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: { locale: string };
+}): Promise<Metadata> => {
   return {
     title: 'Bigspontino',
     description: 'A Desired Meal',
   };
 };
+
  
 export default async function LocaleLayout({
   children,
   params
 }: {
   children: React.ReactNode;
-  params: Promise<{locale: string}>;
+  params: { locale: string }; 
 }) {
-  // Ensure that the incoming `locale` is valid
-  const {locale} = await params;
+  const { locale } = params;
+
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
-  const messages = await getMessages({ locale });
- 
+
   return (
-    <html>
+    <html lang={locale}>
       <body>
-        <NextIntlClientProvider locale={locale}>{children}</NextIntlClientProvider>
+        <NextIntlClientProvider locale={locale}>
+          {children}
+        </NextIntlClientProvider>
       </body>
     </html>
   );
