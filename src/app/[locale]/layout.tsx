ccd961074802c8d0c73ad095ad086977bae8b33c
khadizajarin <khadiza131310@gmail.com>
2025-07-17 19:23:29 +0600

    language setup

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,15 @@ import {NextIntlClientProvider, hasLocale} from 'next-intl';
 import {notFound} from 'next/navigation';
 import {routing} from './../../i18n/routing';
 import '../../app/globals.css';
+import type { Metadata } from 'next';
+import { getMessages } from 'next-intl/server';
+
+export const generateMetadata = async ({ params }: { params: { locale: string } }): Promise<Metadata> => {
+  return {
+    title: 'Bigspontino',
+    description: 'A Desired Meal',
+  };
+};
  
 export default async function LocaleLayout({
   children,
@@ -15,11 +24,12 @@ export default async function LocaleLayout({
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
+  const messages = await getMessages({ locale });
  
   return (
     <html lang={locale}>
       <body>
-        <NextIntlClientProvider>{children}</NextIntlClientProvider>
+        <NextIntlClientProvider locale={locale} messages={messages}>{children}</NextIntlClientProvider>
       </body>
     </html>
   );
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,14 +6,6 @@ import {routing} from '../../i18n/routing';
 import Banner from './banner';
 
 export default function HomePage() {
-  const t = useTranslations('HomePage');
-  const pathname = usePathname(); // current path without locale prefix
-  const router = useRouter();
-
-  const handleLocaleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const newLocale = e.target.value;
-    router.push({pathname}, {locale: newLocale});
-  };
 
   return (
     <div>
